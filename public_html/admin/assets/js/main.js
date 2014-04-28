5378e8c02f4ffb82876d2e4006cbcdbd216eb5ba
Chase Sillevis <chase@sillevis.net>
2014-04-28 11:57:05 +0200

    Add support for 10 mix-panels

--- public_html/admin/assets/js/main.js
+++ public_html/admin/assets/js/main.js
@@ -2,21 +2,22 @@ var primus = Primus.connect('http://node.crowdlighting.com:8080/'),
     colors = Array('#077bf4', '#fedf01', '#c43fcd', '#0099ff', '#000000', '#cccccc', '#ff0000'), //List the hex codes you want to loop through here.
 	color_index = 0,
 	interval = 2500, //How long the color blend transition lasts. (in milliseconds, 1000 = 1 second)
-	pulseInterval = null,
+	pulseInterval = {},
 	current = 0,
     app = {
-		colour: function( colour ) {
+		colour: function( colour, id ) {
 			if( colour ) {
 				data = {};
 				data.colour = ( colour ? colour : '#f4f4f4' );
+				data.id     = id;
 				data.method = ('colour');
 				primus.write(data);
 			}
 		},
-		pulse: function( colour1, colour2, miliseconds ) {
+		pulse: function( colour1, colour2, miliseconds, id ) {
 			if( colour1 && colour2 && miliseconds ) {
-				clearInterval( pulseInterval );
-				pulseInterval = setInterval( function() {
+				clearInterval( pulseInterval.id );
+				pulseInterval.id = setInterval( function() {
 					data = {};
 					if( current == colour1 ) {
 						data.colour = ( colour2 ? colour2 : '#ffffff' );
@@ -25,6 +26,7 @@ var primus = Primus.connect('http://node.crowdlighting.com:8080/'),
 						data.colour = ( colour1 ? colour1 : '#000000' );
 						current = colour1;
 					}
+					data.id     = id;
 					data.method = ('colour');
 					primus.write(data);
 				}, miliseconds );
@@ -36,57 +38,61 @@ var primus = Primus.connect('http://node.crowdlighting.com:8080/'),
 
 $(".picker").spectrum({
     color: "#f4f4f4",
+    flat: true,
     change: function(color) {
-        app.colour('#' + color.toHex());
+        app.colour('#' + color.toHex(), $(this).attr('id').replace('colour', ''));
+        this.value = '#' + color.toHex();
     },
     move: function (color) {
-        app.colour('#' + color.toHex());
+        app.colour('#' + color.toHex(), $(this).attr('id').replace('colour', ''));
+        this.value = '#' + color.toHex();
     }
 });
 
+primus.on( 'colour', function(data) {
+	console.log(data);
+	if( data ) {
+		$( '#colour' + data.id ).spectrum("set", data.colour);
+		$( '#pulse' + data.id + '-1' ).spectrum("set", data.colour);
+	}
+});
+
 $( function() {
-	$( '#colour' ).bind( 'blur', function() {
-		app.colour( $( this ).val() );
-	}).bind( 'keypress', function(e) {
-		var code = (e.keyCode ? e.keyCode : e.which);
-		if( code == 13 ) { //Enter keycode
-			app.colour( $( this ).val() );
-		}
-	});
-	$( '#coloursubmit' ).bind( 'click', function() {
-		app.colour( $( '#colour' ).val() );
-	});
+	for(var i = 1 ; i < 11 ; i++) {
+		$( '#colour' + i ).bind( 'blur', function() {
+			app.colour( $( this ).val(), i );
+		}).bind( 'keypress', function(e) {
+			var code = (e.keyCode ? e.keyCode : e.which);
+			if( code == 13 ) { //Enter keycode
+				app.colour( $( this ).val(), i );
+			}
+		});
 
-
-	$( '#colour1' ).bind( 'keypress', function(e) {
-		var code = (e.keyCode ? e.keyCode : e.which);
-		if( code == 13 ) { //Enter keycode
-			app.pulse( $( '#colour1' ).val(), $( '#colour2' ).val(), ( $( '#miliseconds' ).val() ? $( '#miliseconds' ).val() : 1000 ) );
-		}
-	});
-	$( '#colour2' ).bind( 'keypress', function(e) {
-		var code = (e.keyCode ? e.keyCode : e.which);
-		if( code == 13 ) { //Enter keycode
-			app.pulse( $( '#colour1' ).val(), $( '#colour2' ).val(), ( $( '#miliseconds' ).val() ? $( '#miliseconds' ).val() : 1000 ) );
-		}
-	});
-	$( '#miliseconds' ).bind( 'keypress', function(e) {
-		var code = (e.keyCode ? e.keyCode : e.which);
-		if( code == 13 ) { //Enter keycode
-			app.pulse( $( '#colour1' ).val(), $( '#colour2' ).val(), ( $( '#miliseconds' ).val() ? $( '#miliseconds' ).val() : 1000 ) );
-		}
-	});
-
-	$( '#pulsesubmit' ).bind( 'click', function() {
-		app.pulse( $( '#colour1' ).val(), $( '#colour2' ).val(), ( $( '#miliseconds' ).val() ? $( '#miliseconds' ).val() : 1000 ) );
-	});
-	$( '#pulsestop' ).bind( 'click', function() {
-		clearInterval( pulseInterval );
-	});
-
-	primus.on( 'colour', function(data) {
-		if( data ) {
-			$( '#colour' ).val( data.colour );
-		}
-	} );
+		$( '#pulse'+i+'-1' ).bind( 'keypress', function(e) {
+			var code = (e.keyCode ? e.keyCode : e.which);
+			if( code == 13 ) { //Enter keycode
+				app.pulse( $( '#pulse'+i+'-1' ).val(), $( '#pulse'+i+'-2' ).val(), ( $( '#pulse'+i+'-3' ).val() ? $( '#pulse'+i+'-3' ).val() : 1000 ), i );
+			}
+		});
+		$( '#pulse'+i+'-2' ).bind( 'keypress', function(e) {
+			var code = (e.keyCode ? e.keyCode : e.which);
+			if( code == 13 ) { //Enter keycode
+				app.pulse( $( '#pulse'+i+'-1' ).val(), $( '#pulse'+i+'-2' ).val(), ( $( '#pulse'+i+'-3' ).val() ? $( '#pulse'+i+'-3' ).val() : 1000 ), i );
+			}
+		});
+		$( '#pulse'+i+'-3' ).bind( 'keypress', function(e) {
+			var code = (e.keyCode ? e.keyCode : e.which);
+			if( code == 13 ) { //Enter keycode
+				app.pulse( $( '#pulse'+i+'-1' ).val(), $( '#pulse'+i+'-2' ).val(), ( $( '#pulse'+i+'-3' ).val() ? $( '#pulse'+i+'-3' ).val() : 1000 ), i );
+			}
+		});
+		$( '#pulsesubmit'+i ).bind( 'click', function() {
+			var id = $(this).attr('id').replace('pulsesubmit', '');
+			app.pulse( $( '#pulse'+id+'-1' ).val(), $( '#pulse'+id+'-2' ).val(), ( $( '#pulse'+id+'-3' ).val() ? $( '#pulse'+id+'-3' ).val() : 1000 ), id );
+		});
+		$( '#pulsestop'+i ).bind( 'click', function() {
+			var id = $(this).attr('id').replace('pulsestop', '');
+			clearInterval( pulseInterval.id );
+		});
+	}
 });
